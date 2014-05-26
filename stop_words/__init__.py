54a3b85de151b2b7da5e7ca98a332696d35c88e0
Alireza Savand <alireza.savand@gmail.com>
2014-05-26 11:22:20 +0400

    Get the stop words in a very very nice way!

--- stop_words/__init__.py
+++ stop_words/__init__.py
@@ -1,4 +1,8 @@
+import os
+
 __VERSION__ = (2014, 5, 26)
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+STOP_WORDS_DIR = os.path.join(CURRENT_DIR, 'stop-words/')
 
 
 def get_version():
@@ -14,6 +18,6 @@ def get_stop_words(language):
 
     :rtype: list
     """
-    with open('stop-words/{}.txt'.format(language)) as lang_file:
+    with open('{0}{1}.txt'.format(STOP_WORDS_DIR, language)) as lang_file:
         lines = lang_file.readlines()
         return [str(line.strip()).decode('utf-8') for line in lines]
