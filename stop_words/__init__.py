c2120097a4a0420e5445f0a03ff9da874a1a8c7d
Alireza Savand <alireza.savand@gmail.com>
2014-05-26 10:43:05 +0400

    Add get_stop_words

--- stop_words/__init__.py
+++ stop_words/__init__.py
@@ -6,3 +6,14 @@ def get_version():
     :rtype: basestring
     """
     return ".".join(str(v) for v in __VERSION__)
+
+
+def get_stop_words(language):
+    """
+    :type language: basestring
+
+    :rtype: list
+    """
+    with open('stop-words/{}.txt'.format(language)) as lang_file:
+        lines = lang_file.readlines()
+        return [str(line.strip()).decode('utf-8') for line in lines]
