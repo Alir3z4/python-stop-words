cf017b7653b091c16b8e805ddfceb75433c9c5f8
Fantomas42 <fantomas42@gmail.com>
2015-01-13 15:59:44 +0100

    Implement cache for successive calls

--- stop_words/__init__.py
+++ stop_words/__init__.py
@@ -3,6 +3,7 @@ import os
 __VERSION__ = (2014, 5, 26)
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 STOP_WORDS_DIR = os.path.join(CURRENT_DIR, 'stop-words/')
+STOP_WORDS_CACHE = {}
 
 LANGUAGE_MAPPING = {
     'ar': 'arabic',
@@ -24,6 +25,7 @@ LANGUAGE_MAPPING = {
 }
 
 AVAILABLE_LANGUAGES = LANGUAGE_MAPPING.values()
+
 
 def get_version():
     """
@@ -48,11 +50,21 @@ def get_stop_words(language):
         pass
 
     if language not in AVAILABLE_LANGUAGES:
-        raise StopWordError('%s language is unavailable')
+        raise StopWordError('"%s" language is unavailable.' % language)
+
+    if language in STOP_WORDS_CACHE:
+        return STOP_WORDS_CACHE[language]
 
     try:
-        with open('{0}{1}.txt'.format(STOP_WORDS_DIR, language)) as lang_file:
-            lines = lang_file.readlines()
-            return [str(line.strip()).decode('utf-8') for line in lines]
+        language_filename = '{0}{1}.txt'.format(STOP_WORDS_DIR, language)
+        with open(language_filename) as language_file:
+            stop_words = [str(line.strip()).decode('utf-8')
+                          for line in language_file.readlines()]
     except IOError:
-        raise StopWordError('%s file is unreadable, check your installation')
+        raise StopWordError(
+            '"%s" file is unreadable, check your installation.' %
+            language_filename)
+
+    STOP_WORDS_CACHE[language] = stop_words
+
+    return stop_words
