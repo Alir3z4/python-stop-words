8de54ea6edd614303996398925802524729efbab
Alireza Savand <alireza.savand@gmail.com>
2018-07-23 23:45:36 +0300

    Add correct classifiers

--- setup.py
+++ setup.py
@@ -18,9 +18,17 @@ setup(
         ]
     },
     classifiers=[
-        "Programming Language :: Python",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development"
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+        'Development Status :: 6 - Mature',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Filters',
     ],
 )
